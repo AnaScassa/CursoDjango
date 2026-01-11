deb98d42cc30ffdb7dc8e83ceaebce5b7e1331d7
anascassa <anacaroline25190@gmail.com>
2026-01-11 20:17:02 -0300

    aula08

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 # HTTP REQUEST
 def my_view(request):
@@ -11,3 +13,6 @@ urlpatterns = [
     path('sobre/', my_view),
     path('', include('recipes.urls')),    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
