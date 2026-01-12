a99c232521753164cb763d0c5b3d81a62f1e064d
anascassa <anacaroline25190@gmail.com>
2026-01-12 16:40:49 -0300

    aula09.1

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        assert 1 == 1, 'Just a test to check if pytest is working correctly.'  
+
+    def test_category_url_is_corret(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, 'recipes/category/1/')
