aa8154b30d438aaa61a3d6e493f47fdd2ab06fc2
anascassa <anacaroline25190@gmail.com>
2026-01-21 18:20:46 -0300

    aula10.2

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,7 +5,7 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('recipes/search/', lambda request: ..., name="search"),
+    path('recipes/search/', views.search, name="search"),
     path('recipes/<int:id>/', views.recipe, name='recipe'),
     path('category/<int:category_id>/', views.category, name='category'),
 
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,9 @@
+from urllib import request
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 from django.http.response import Http404
+from django.db.models import Q
 
 def home(request):
     recipes = Recipe.objects.filter(
@@ -38,7 +40,20 @@ def recipe(request, id):
     })
 
 def search(request):
-    search_term = request.GET.get('q')
+    search_term = request.GET.get('q', '').strip()
+
     if not search_term:
         raise Http404()
-    return render(request, 'recipes/pages/search.html')
+    
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True,
+    ).order_by('-id')
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes,
+    })
